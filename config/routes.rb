ae773c93f046caa6581ef2c1887ba514f0a796c0
saulcoria <coriasaul@gmail.com>
2018-11-02 18:32:14 -0300

    Add director and producer dashboard

--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,17 @@
 Rails.application.routes.draw do
   namespace :admin do
-    resources :castings
     resources :movies
     resources :people
+    resources :directors_movies
+    resources :producers_movies
+    resources :castings
 
-    root to: "castings#index"
+    root to: "movies#index"
   end
 
   namespace :v1 do
     resources :movies
     resources :people
   end
-
-  resources :castings
-  resources :movies
-  resources :people
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
